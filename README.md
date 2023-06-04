cccea9890b2374cdbeb10574ab7a2378fc3c6551
haatmax24 <haatmax24@gmail.com>
2023-06-04 21:56:33 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ecommerce web site haatmax
+
+## Lessons
+
+1. install all file and tools
+2. crate typeScript React app by vite
+3. create Git Repository
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>haatmax24</header>
+      <main></main>
+      <footer>All right reserved</footer>
+    </div>
+  );
+}
+
+export default App;
