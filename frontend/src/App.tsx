6180f124417ecb29a7ac440566595c14c644557c
haatmax24 <haatmax24@gmail.com>
2023-06-05 17:37:28 +0600

    lesstion 7 done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Container, Navbar, Nav, Row, Col } from 'react-bootstrap';
-import { sampleProducts } from './data';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -23,19 +23,7 @@ function App() {
       </header>
       <main>
         <Container className="mt-3">
-          <Row>
-            {sampleProducts.map((product) => (
-              <Col key={product.slug} sm={6} md={4} lg={3}>
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className="product-image"
-                />
-                <h2>{product.name}</h2>
-                <p>৳{product.price}</p>
-              </Col>
-            ))}
-          </Row>
+          <Outlet />
         </Container>
       </main>
 
