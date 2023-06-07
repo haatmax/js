1dbfdba1e269e9b4f2783bca34fc4601b73df2fd
haatmax24 <haatmax24@gmail.com>
2023-06-08 04:00:42 +0600

    error product page

--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -2,7 +2,16 @@ import { Helmet } from 'react-helmet-async';
 
 export default function ProductPage() {
   return (
-    <div>
+   isLoading ? (
+      <LoadingBox />)
+      : 
+      error   ? (
+        <MessageBox variant="danger">{getError(error as ApiError)}</MessageBox>
+      )
+      : !product ? (
+        <MessageBox variant="danger">Product Not Found</MessageBox>
+      ):
+    (<div>
       <Helmet>
         <title>Product Page</title>
       </Helmet>
